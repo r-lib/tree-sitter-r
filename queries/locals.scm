7030d7a483530ae775b5894cc15cbcd1562ff714
Davis Vaughan <davis@posit.co>
2024-03-08 13:16:27 -0500

    Update queries

--- queries/locals.scm
+++ queries/locals.scm
@@ -1,12 +1,12 @@
 ; locals.scm
 
-(function) @local.scope
+(function_definition) @local.scope
 
 (argument  name: (identifier) @local.definition)
 (parameter name: (identifier) @local.definition)
 
-("<-" lhs: (identifier) @local.definition)
-("="  lhs: (identifier) @local.definition)
-("->" rhs: (identifier) @local.definition)
+(left_assignment lhs: (identifier) @local.definition)
+(equals_assignment  lhs: (identifier) @local.definition)
+(right_assignment rhs: (identifier) @local.definition)
 
 (identifier) @local.reference
