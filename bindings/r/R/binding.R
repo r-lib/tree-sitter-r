685d6e3dc4a021fd89a9db3f291e25492e80fe39
Davis Vaughan <davis@posit.co>
2024-03-11 15:41:14 -0400

    More notes

--- bindings/r/R/binding.R
+++ bindings/r/R/binding.R
@@ -10,7 +10,8 @@
 #' language()
 language <- function() {
   # TODO: Possibly export wrapper from tree-sitter R bindings package for
-  # `new_language()` that all grammar binding packages could use?
+  # `new_language()` that all grammar binding packages could use? Would also
+  # provide a print method and `is_language()` method.
   out <- .Call(ffi_language)
   class(out) <- c("tree_sitter_r_language", "tree_sitter_language")
   out
