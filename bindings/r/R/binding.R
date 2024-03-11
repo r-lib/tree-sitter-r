4601d0db445f69fec2c45c226d60640ab3e4ed9e
Davis Vaughan <davis@posit.co>
2024-03-11 15:38:44 -0400

    Actually wire up external pointer

--- bindings/r/R/binding.R
+++ bindings/r/R/binding.R
@@ -1,3 +1,15 @@
+#' tree-sitter language for R
+#'
+#' `language()` returns an external pointer to the tree-sitter language
+#' object for R.
+#'
+#' @returns An external pointer.
+#'
+#' @export
+#' @examples
+#' language()
 language <- function() {
-  .Call(ffi_language)
+  out <- .Call(ffi_language)
+  class(out) <- c("tree_sitter_r_language", "tree_sitter_language")
+  out
 }
--- bindings/r/src/binding.c
+++ bindings/r/src/binding.c
@@ -2,6 +2,19 @@
 #include <R.h>
 #include <Rinternals.h>
 
+#include "tree_sitter/parser.h"
+
+// `parser.c`
+extern const TSLanguage* tree_sitter_r(void);
+
 SEXP ffi_language(void) {
-  return R_NilValue;
+  static SEXP language = NULL;
+
+  if (language == NULL) {
+    const TSLanguage* pointer = tree_sitter_r();
+    language = R_MakeExternalPtr((void*) pointer, R_NilValue, R_NilValue);
+    R_PreserveObject(language);
+  }
+
+  return language;
 }
