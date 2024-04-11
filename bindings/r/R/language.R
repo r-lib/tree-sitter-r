f54d434f073dc5739cdef00dc0ce81d025777227
Davis Vaughan <davis@posit.co>
2024-04-11 15:23:42 -0400

    Use standalone file

--- bindings/r/R/language.R
+++ bindings/r/R/language.R
@@ -10,15 +10,5 @@
 #' language()
 language <- function() {
   pointer <- .Call(ffi_language)
-  new_language("r", pointer)
-}
-
-new_language <- function(name, pointer) {
-  stopifnot(is.character(name), length(name) == 1L, !is.na(name))
-  stopifnot(typeof(pointer) == "externalptr")
-
-  out <- list(name = name, pointer = pointer)
-  class(out) <- "tree_sitter_language"
-
-  out
+  new_language(pointer, name = "r")
 }
