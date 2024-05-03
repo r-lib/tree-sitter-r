187177fec22f5c2569806e7c5b10e956c47b0b19
Davis Vaughan <davis@posit.co>
2024-05-03 15:20:31 -0400

    Do recommended updates to `build.rs`

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,22 +1,19 @@
 fn main() {
     let src_dir = std::path::Path::new("src");
 
-    let mut config = cc::Build::new();
-    config.include(&src_dir);
-    config
-        .flag_if_supported("-Wno-unused-parameter")
-        .flag_if_supported("-Wno-unused-but-set-variable")
-        .flag_if_supported("-Wno-trigraphs");
+    let mut c_config = cc::Build::new();
+    c_config.std("c11").include(src_dir);
 
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
 
     let parser_path = src_dir.join("parser.c");
-    let scanner_path = src_dir.join("scanner.c");
+    c_config.file(&parser_path);
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
+
+    let scanner_path = src_dir.join("scanner.c");
+    c_config.file(&scanner_path);
     println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
-    config.file(&parser_path);
-    config.file(&scanner_path);
-    
-    config.compile("parser-scanner");
+
+    c_config.compile("tree-sitter-r");
 }
