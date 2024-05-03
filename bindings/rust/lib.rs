eeca91743d54364176652ae13894cb0ad0790b81
Davis Vaughan <davis@posit.co>
2024-05-03 15:25:35 -0400

    Do recommended updates to `lib.rs`. Including adding highlights, locals, and tags objects.

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,4 +1,4 @@
-//! This crate provides r language support for the [tree-sitter][] parsing library.
+//! This crate provides R language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
@@ -7,8 +7,9 @@
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
 //! let language = tree_sitter_r::language();
-//! parser.set_language(&language).expect("Error loading r grammar");
+//! parser.set_language(&language).expect("Error loading R grammar");
 //! let tree = parser.parse(code, None).unwrap();
+//! assert!(!tree.root_node().has_error());
 //! ```
 //!
 //! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
@@ -32,23 +33,21 @@ pub fn language() -> Language {
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
+pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
-// Uncomment these to include any queries that this grammar contains
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
 
-// pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
+
+pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
-        let language = super::language();
         parser
-            .set_language(&language)
-            .expect("Error loading r language");
+            .set_language(&super::language())
+            .expect("Error loading R language");
     }
 }
