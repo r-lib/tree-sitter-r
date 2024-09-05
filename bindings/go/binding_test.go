b6e5ab7775ac6cd537a9c5f21bd5061cf014cd4c
Hendrik van Antwerpen <hendrikvanantwerpen@github.com>
2024-09-05 16:03:07 +0200

    update bindings

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_r_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-r"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_r "github.com/tree-sitter/tree-sitter-r/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
