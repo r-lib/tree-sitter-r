3e1de04fa1799c066d62d912bcb849bb2c386047
Davis Vaughan <davis@posit.co>
2025-05-27 11:17:43 -0400

    Update to tree-sitter v0.24.7 (#169)
    * `tree-sitter init`
    
    * Swap author ordering for `package.json` generation purposes
    
    * Regenerate `package.json`
    
    * Regen C related files
    
    * Update go related files
    
    * Regenerate node related files
    
    * Regenerate Python related files
    
    * Regenerate Rust related files
    
    * Regenerate Swift related files
    
    * Regen editor / git related files
    
    Selective acceptance of `.gitattributes` changes, I think it is nice to actually see these diffs
    
    * Update LICENSE year
    
    * `tree-sitter generate`
    
    * Remove outdated setting
    
    * Add Jim to `tree-sitter.json` authors list
    
    * Pin tree-sitter version in CI
    
    * Changelog bullet

--- bindings/go/binding.go
+++ bindings/go/binding.go
@@ -2,7 +2,7 @@ package tree_sitter_r
 
 // #cgo CFLAGS: -std=c11 -fPIC
 // #include "../../src/parser.c"
-// #include "../../src/scanner.c"
+// // NOTE: if your language has an external scanner, add it here.
 import "C"
 
 import "unsafe"
